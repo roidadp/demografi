531988e3efeceac090d2763ab53df63d7232fa0b
roidadp <roidadipakmal@gmail.com>
2023-01-31 15:55:06 +0700

    first commit

---
+++ app/Views/dashboard.php
@@ -0,0 +1,36 @@
+<?= $this->include('header'); ?>
+<?= $this->include('sidemenu'); ?>
+
+  <main id="main" class="main">
+
+    <div class="pagetitle">
+      <h1>Dashboard</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+
+        <!-- Left side columns -->
+        <div class="col-lg-8">
+          <div class="row">
+
+              </div><!-- End sidebar recent posts-->
+
+            </div>
+          </div><!-- End News & Updates -->
+
+        </div><!-- End Right side columns -->
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+
+  <?= $this->include('footer'); ?>
